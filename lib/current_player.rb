eeaf4b9969a7e6a05d87562977b35e459d65e1dc
Emily Willow <emily.shapiera@gmail.com>
2020-11-24 01:34:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,7 @@ def turn_count(board)
 counter = 0
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   board.each do |board_number|
+  end
     if board_number == "X" or if board_number == "O"
       counter += 1
 end
